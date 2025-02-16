e81e67a1e26b930a9389dc7b7f7c83a89d3a2e92
kitsuiwebster <raphmartin1999@gmail.com>
2025-02-16 18:21:12 +0100

    feat: hackaprompt article

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './articles.component.html',
-  styleUrl: './articles.component.scss'
+  styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent {
-
+  articles = [
+    { title: 'HackAPrompt 2023', description: 'HackAPrompt 2023 : How I tricked the AI models.', link: '/hackaprompt' }
+  ];
 }
