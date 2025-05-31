ed6839724fde30387fe336d9ef98b6c9a591b432
kitsuiwebster <raphmartin1999@gmail.com>
2025-05-31 18:34:41 +0200

    feat: stats style

--- src/app/pages/shisui/shisui.component.ts
+++ src/app/pages/shisui/shisui.component.ts
@@ -1,7 +1,8 @@
+// shisui.component.ts
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NgxChartsModule, Color, ScaleType } from '@swimlane/ngx-charts';
 
 interface Entry {
   date: string;
@@ -21,9 +22,29 @@ export class ShisuiComponent implements OnInit {
   // Utiliser une chaîne simple pour la position de la légende
   legendPosition = 'right';
   
-  // Utiliser une chaîne pour le schéma de couleurs
-  colorScheme = 'vivid';
-  view: [number, number] = [800, 400];
+  // Palette de couleurs arc-en-ciel étendue
+  colorScheme: Color = {
+    name: 'rainbowColors',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: [
+      '#FF0000', '#FF3300', '#FF6600', '#FF9900', '#FFCC00', '#FFFF00', // Rouges et jaunes
+      '#CCFF00', '#99FF00', '#66FF00', '#33FF00', '#00FF00', // Verts
+      '#00FF33', '#00FF66', '#00FF99', '#00FFCC', '#00FFFF', // Cyans
+      '#00CCFF', '#0099FF', '#0066FF', '#0033FF', '#0000FF', // Bleus
+      '#3300FF', '#6600FF', '#9900FF', '#CC00FF', '#FF00FF', // Violets
+      '#FF00CC', '#FF0099', '#FF0066', '#FF0033', // Retour au rouge
+      '#E6005C', '#B3005C', '#800040', '#400030', '#000000', // Dégradé vers le noir
+      '#19376D', '#A5D7E8', '#576CBC', '#0B2447', '#7B8FA1', // Bleus variés
+      '#82AAE3', '#91D8E4', '#BFEAF5', '#FFF9DE', '#A5C0DD', // Tons pastels
+      '#F6F7C1', '#C6EBC5', '#51557E', '#816797', '#D6D5A8', // Tons terreux
+      '#1B9C85', '#E8F6EF', '#B8E7E1', '#D2E0FB', '#AFB4FF', // Verts et bleus clairs
+      '#FFCEFE', '#FFDEB4', '#FFB4B4', '#B46060', '#4E6C50', // Roses et bruns
+      '#F1F0C0', '#7743DB', '#5EAAA8', '#F05454', '#9E7676'  // Couleurs diverses
+    ]
+  };
+  
+  view: [number, number] = [1200, 800];
   
   entries: Entry[] = [];
   cityStats: { name: string; value: number }[] = [];
